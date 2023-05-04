cd78de3f9076115d85daf3f4d8e35a9cdcaf2988
Okombi Erhyck <okombie@gmail.com>
2023-05-04 21:02:07 +0300

    Dfdrff

--- my-app/src/orders.jsx
+++ my-app/src/orders.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './OrdersPage.css';
 
 const OrdersPage = () => {
   const [orders, setOrders] = useState([]);
@@ -16,41 +15,64 @@ const OrdersPage = () => {
       });
   }, []);
 
+  const markAsDelivered = (orderId) => {
+    axios
+      .put(`/orders/${orderId}/delivered`)
+      .then((response) => {
+        // Update the orders state to reflect the change
+        const updatedOrders = orders.map((order) => {
+          if (order._id === orderId) {
+            return {
+              ...order,
+              delivered: true,
+            };
+          }
+          return order;
+        });
+        setOrders(updatedOrders);
+      })
+      .catch((error) => {
+        console.error('Failed to mark order as delivered: ', error);
+      });
+  };
+
   if (!orders || orders.length === 0) {
     return <p>No orders found.</p>;
   }
 
   return (
-    <div className="orders-container">
-      <h1 className="orders-title">Orders</h1>
+    <div>
+      <h1>Orders</h1>
       {orders.map((order, index) => (
         <div key={index} className="order">
-          <h3 className="order-number">Order #{index + 1}</h3>
+          <h3>Order #{index + 1}</h3>
           <div className="delivery-details">
             <strong>Delivery Details:</strong>
-            <p><span>First Name:</span> {order.firstName}</p>
-            <p><span>Last Name:</span> {order.lastName}</p>
-            <p><span>Email:</span> {order.email}</p>
-            <p><span>Address:</span> {order.address}</p>
-            <p><span>City:</span> {order.city}</p>
-            <p><span>ZIP Code:</span> {order.zipCode}</p>
+            <p>First Name: {order.firstName}</p>
+            <p>Last Name: {order.lastName}</p>
+            <p>Email: {order.email}</p>
+            <p>Address: {order.address}</p>
+            <p>City: {order.city}</p>
+            <p>ZIP Code: {order.zipCode}</p>
           </div>
           <div className="cart-places">
             <strong>Cart Places:</strong>
             {order.cartItems && order.cartItems.length > 0 ? (
               order.cartItems.map((place, placeIndex) => (
-                <div key={placeIndex} className="place">
+                <div className="place" key={placeIndex}>
                   <p className="place-title">{place.title} - Lei{place.km}</p>
-                  {/* Access other necessary fields of the place data */}
                   <p className="place-description">{place.description}</p>
                   <p className="place-price">{place.price}</p>
-                  {/* Add more fields as needed */}
                 </div>
               ))
             ) : (
               <p>No places found for this order.</p>
             )}
           </div>
+          {!order.delivered && (
+            <button onClick={() => markAsDelivered(order._id)}>Mark as Delivered</button>
+          )}
+          {order.delivered && <p>Order Delivered</p>}
         </div>
       ))}
     </div>
