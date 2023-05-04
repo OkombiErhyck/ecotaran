78b026199c2f70e417349d0edf80c4def01b36d7
Okombi Erhyck <okombie@gmail.com>
2023-05-04 21:37:32 +0300

    dfvdf

--- my-app/src/orders.jsx
+++ my-app/src/orders.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import './OrdersPage.css';
+
 const OrdersPage = () => {
   const [orders, setOrders] = useState([]);
 
@@ -31,12 +33,15 @@ const OrdersPage = () => {
   }
 
   return (
-    <div>
+    <div className="orders-container">
       <h1>Orders</h1>
       {orders.map((order, index) => (
-        <div key={index} className={`order ${order.status === 'Delivered' ? 'delivered' : ''}`}>
+        <div
+          key={index}
+          className={`order ${order.status === 'Delivered' ? 'delivered' : ''}`}
+        >
           <h3>Order #{index + 1}</h3>
-          <div>
+          <div className="delivery-details">
             <strong>Delivery Details:</strong>
             <p>First Name: {order.firstName}</p>
             <p>Last Name: {order.lastName}</p>
@@ -45,15 +50,14 @@ const OrdersPage = () => {
             <p>City: {order.city}</p>
             <p>ZIP Code: {order.zipCode}</p>
           </div>
-          <div>
+          <div className="cart-places">
             <strong>Cart Places:</strong>
             {order.cartItems && order.cartItems.length > 0 ? (
               order.cartItems.map((place, placeIndex) => (
-                <div key={placeIndex}>
-                  <p>{place.title} - Lei{place.km}</p>
-                  {/* Access other necessary fields of the place data */}
-                  <p>{place.description}</p>
-                  <p>{place.price}</p>
+                <div key={placeIndex} className="place">
+                  <p className="place-title">{place.title} - Lei{place.km}</p>
+                  <p className="place-description">{place.description}</p>
+                  <p className="place-price">{place.price}</p>
                   {/* Add more fields as needed */}
                 </div>
               ))
@@ -62,10 +66,11 @@ const OrdersPage = () => {
             )}
           </div>
           <button
+            className="mark-delivered-btn"
             onClick={() => handleClickDelivered(order._id)}
             disabled={order.status === 'Delivered'}
           >
-            Mark as Delivered
+            {order.status === 'Delivered' ? 'Delivered' : 'Mark as Delivered'}
           </button>
         </div>
       ))}
