bd6cf2cdb990c0478241d60ee88c2f22f98408b4
Okombi Erhyck <okombie@gmail.com>
2023-06-08 01:01:53 +0300

    vdfvdf

--- my-app/src/UserContext.jsx
+++ my-app/src/UserContext.jsx
@@ -1,24 +1,47 @@
-import {createContext, useEffect, useState} from "react";
+import React, { createContext, useEffect, useState, useContext } from "react";
 import axios from "axios";
-import {data} from "autoprefixer";
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}) {
-  const [user,setUser] = useState(null);
-  const [ready,setReady] = useState(false);
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [ready, setReady] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState(0);
+
   useEffect(() => {
     if (!user) {
-      axios.get('/profile').then(({data}) => {
+      axios.get('/profile').then(({ data }) => {
         setUser(data);
         setReady(true);
       });
     }
   }, []);
 
-  
+  useEffect(() => {
+    if (cart.length > 0) {
+      const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+      setCartQuantity(totalQuantity);
+    } else {
+      setCartQuantity(0);
+    }
+  }, [cart]);
+
+  const addToCart = (place, quantity) => {
+    const updatedPlace = { ...place, quantity: quantity || 1 };
+    const placeIndex = cart.findIndex((item) => item._id === place._id);
+
+    if (placeIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[placeIndex].quantity += quantity;
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, updatedPlace]);
+    }
+  };
+
   return (
-    <UserContext.Provider value={{user,setUser,ready}}>
+    <UserContext.Provider value={{ user, setUser, ready, cart, setCart, cartQuantity, addToCart }}>
       {children}
     </UserContext.Provider>
   );
--- my-app/src/navbar.jsx
+++ my-app/src/navbar.jsx
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import MenuImg from "./images/menu.png";
 import cos from "./images/cos.png";
+import { UserContext } from "./UserContext";
 
 import "./navbar.css";
 
 const NavBar = () => {
   const [navbar, setNavbar] = useState(false);
-  const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem("cart")) || []
-  );
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [cartQuantity, setCartQuantity] = useState(cartItems.length);
+  const { cart } = useContext(UserContext); // Access the cart state from the UserContext
 
   const toggleNavbar = () => {
     setMobileMenuOpen(!mobileMenuOpen);
@@ -32,17 +29,7 @@ const NavBar = () => {
     };
   }, []);
 
-  const updateCartQuantity = () => {
-    const updatedCartQuantity = cartItems.length;
-    setCartQuantity(updatedCartQuantity);
-  };
-
-  const addToCart = (item) => {
-    const updatedCartItems = [...cartItems, item];
-    setCartItems(updatedCartItems);
-    localStorage.setItem("cart", JSON.stringify(updatedCartItems));
-    updateCartQuantity();
-  };
+  const cartQuantity = cart.reduce((total, item) => total + item.quantity, 0); // Calculate the total cart quantity
 
   return (
     <>
