1e6a9cab719d149f3b607bc00b0f14874d092842
Okombi Erhyck <92018794+OkombiErhyck@users.noreply.github.com>
2024-03-14 13:57:45 +0200

    Update navbar.jsx

--- my-app/src/navbar.jsx
+++ my-app/src/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import MenuImg from "./images/menu.png";
 
@@ -14,6 +14,7 @@ const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [cartQuantity, setCartQuantity] = useState(cartItems.length);
   const [refreshKey, setRefreshKey] = useState(0); // Key for forcing component refresh
+  const [loginLinkColor, setLoginLinkColor] = useState("black"); // Initial color
   const cartLinkRef = useRef(null);
 
   const toggleNavbar = () => {
@@ -44,33 +45,22 @@ const NavBar = () => {
     updateCartQuantity();
   }, [cartItems]);
 
-  useEffect(() => {
-    const handleStorageChange = (event) => {
-      if (event.key === "cart") {
-        const updatedCartItems = JSON.parse(event.newValue) || [];
-        setCartItems(updatedCartItems);
-        forceCartLinkUpdate();
-      }
-    };
+  
 
-    window.addEventListener("storage", handleStorageChange);
-
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
-
-  const forceCartLinkUpdate = () => {
-    if (cartLinkRef.current) {
-      // Access the current property of the ref to get the CartLink component instance
-      cartLinkRef.current.forceUpdate();
-    }
-  };
+   
 
   useEffect(() => {
     if (window.location.pathname !== "/") {
       window.scrollTo(0, 0);
     }
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoginLinkColor("white");
+    }, 5000);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -82,11 +72,13 @@ const NavBar = () => {
             : "navbar navbar-expand-lg navbar-expand-md fixed-top"
         }
       >
-       
         <div className="navbar-middle">
           <a href="/" className="navbar-brand">
             <span>UP</span>Recruitment
           </a>
+          <Link to="/login" className="navbar-brand" style={{ color: loginLinkColor }}>
+            Login
+          </Link>
         </div>
         
         <div
