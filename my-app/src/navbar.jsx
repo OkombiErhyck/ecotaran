6b26ff25ecf9e13ce43a258a09eeee1fd4127995
Okombi Erhyck <92018794+OkombiErhyck@users.noreply.github.com>
2025-05-19 09:52:32 +0300

    Update navbar.jsx

--- my-app/src/navbar.jsx
+++ my-app/src/navbar.jsx
@@ -1,20 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import MenuImg from "./images/menu.png";
-import CartLink from "./CartLink";
+import { UserContext } from "./UserContext";  // importă contextul utilizatorului
 import "./navbar.css";
 
 const NavBar = () => {
   const [navbar, setNavbar] = useState(false);
-  const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem("cart")) || []
-  );
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [cartQuantity, setCartQuantity] = useState(cartItems.length);
-  const [refreshKey, setRefreshKey] = useState(0);
-  const [loginLinkColor, setLoginLinkColor] = useState("black");
-  const cartLinkRef = useRef(null);
   const location = useLocation();
+
+  const { user } = useContext(UserContext);  // preia user din context
 
   const toggleNavbar = () => {
     setMobileMenuOpen((prev) => !prev);
@@ -34,22 +29,9 @@ const NavBar = () => {
   }, []);
 
   useEffect(() => {
-    setCartQuantity(cartItems.length);
-    setRefreshKey((prevKey) => prevKey + 1);
-  }, [cartItems]);
-
-  useEffect(() => {
     if (window.location.pathname !== "/") {
       window.scrollTo(0, 0);
     }
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoginLinkColor("white");
-    }, 1000);
-
-    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -59,7 +41,6 @@ const NavBar = () => {
           <Link to="/" className="navbar-brand">
             <span>UP</span>Recruitment
           </Link>
-           
         </div>
 
         {/* Mobile Menu Toggle Button */}
@@ -69,33 +50,37 @@ const NavBar = () => {
 
         {/* Navigation Links */}
         <div className={`collapse navbar-collapse justify-content-end ${mobileMenuOpen ? "show" : ""}`}>
-        <ul className="navbar-nav">
-          
-        <Link to="/login" className="navbar-brand"  onClick={() => setMobileMenuOpen(false)}>
-            Login
-          </Link>
-           
-            <li className={`nav-item ${location.pathname === "/" ? "active" : ""}`}>
-              <Link to="/" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
-                
-              </Link>
-            </li>
+          <ul className="navbar-nav">
+
+            {user ? (
+              <li className={`nav-item ${location.pathname === "/userpage" ? "active" : ""}`}>
+                <Link to="/userpage" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
+                  {user.name}
+                </Link>
+              </li>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/login" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signup" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
+                    Signup
+                  </Link>
+                </li>
+              </>
+            )}
+
+            {/* Alte link-uri din navbar */}
             <li className={`nav-item ${location.pathname === "/about" ? "active" : ""}`}>
               <Link to="/about" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
-                
+                About
               </Link>
             </li>
-            <li className={`nav-item ${location.pathname === "/services" ? "active" : ""}`}>
-              <Link to="/services" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
-                
-              </Link>
-            </li>
-            <li className={`nav-item ${location.pathname === "/contact" ? "active" : ""}`}>
-              <Link to="/contact" className="navbar-brand" onClick={() => setMobileMenuOpen(false)}>
-                
-              </Link>
-            </li>
-             
+
+            {/* ... restul link-urilor */}
           </ul>
         </div>
       </nav>
