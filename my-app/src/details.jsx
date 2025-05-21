b01109de5468c2d138ea97f7b47014c96cbd971b
Okombi Erhyck <92018794+OkombiErhyck@users.noreply.github.com>
2025-05-21 15:35:39 +0300

    Update details.jsx

--- my-app/src/details.jsx
+++ my-app/src/details.jsx
@@ -1,90 +1,110 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import "./IndexPage.css"
-import { Link } from "react-router-dom";
-import Image from "./image";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBox, faCalendarAlt,faRoad } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Myadds.css';
+import Image from './image';
+import { Link } from 'react-router-dom';
 
-
-
-
-export default function Details() {
+export default function PlacesPage() {
   const [places, setPlaces] = useState([]);
+  const [selectedMarca, setSelectedMarca] = useState("");
+  const [selectedModel, setSelectedModel] = useState("");
 
   useEffect(() => {
-    const fetchData = () => {
-      axios.get("/places").then(response => { 
-        setPlaces(response.data);
-      });
-    };
-    fetchData(); // Fetch data initially
-    const intervalId = setInterval(fetchData, 10 * 60 * 1000); // Fetch data every 10 minutes
-    return () => clearInterval(intervalId); // Cleanup function to clear the interval
-  }, []);
-
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios.get('/places').then(response => {
-      const sortedPlaces = response.data.sort((a, b) => new Date(b.date) - new Date(a.date));
-      setPlaces(sortedPlaces);
-      setLoading(false); // Set loading to false when the data is fetched
+    axios.get('/user-places').then(({ data }) => {
+      // Filter places that include "Personal" in the 'marca' field
+      const filtered = data.filter(place =>
+        place.marca && place.marca.toLowerCase().includes("personal")
+      );
+      setPlaces(filtered);
     });
   }, []);
 
-  const shuffleArray = (array) => {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
+  const handleModelSelect = (event) => setSelectedModel(event.target.value);
+
+  const handleDelete = (event, id) => {
+    event.preventDefault();
+    axios.delete(`/places/${id}`).then(() => {
+      setPlaces((prevPlaces) => prevPlaces.filter((place) => place._id !== id));
+    });
   };
 
-  const limitedPlaces = shuffleArray(places).slice(0, 3); // display 3 random posts
+  const resetFilters = () => {
+    setSelectedMarca("");
+    setSelectedModel("");
+  };
 
-  return(
-    <div className="main2"> 
-      <div className="container">
-      {loading ? (
-          <div className="loader">
-        <div className="spinner"> </div>
-        <span className="loading-text">EcoTaran</span>
-      </div>
-       ) : (
-        <div className="details container">
-        <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
-            {limitedPlaces.length > 0 && limitedPlaces.map(place => ( 
-              <Link to={"/place/" + place._id} key={place._id} className="link-no-underline" >
-              <div className="col ">
-                  <div className="box card-body p-0  shadow-sm mb-5">
-                    {place.photos.length > 0 && (
-                      <Image src={place.photos[0]} className="img-fluid" style={{height: "270px", width: "100%", objectFit: "cover"}}/>
-                    )}
-                    <div className="box_content">
-                     <h4>  {place.title}</h4>
-                     <div className="row pl-2 pr-2">
-    <div > 
-      {place.putere}
-  </div>
- 
-                     
+  const filteredPlaces = selectedMarca
+    ? places.filter(place => place.marca === selectedMarca)
+    : places;
 
+  return (
+    <>
+      <div className="top" style={{ marginTop: "80px" }}></div>
 
-  <button style={{background : "#cccccc00", color : "var(--main)"}} className="btn1">Detalii</button>
-                    </div>
-                  </div>
-                </div>
-                </div>
-              </Link>
-            ))}
+      <div className="main2">
+        <div className="filter-container">
+
+          <div className="marcas-buttons">
+            <button
+              onClick={() => setSelectedMarca('Personal Ro')}
+              className={`marca-button ${selectedMarca === 'Personal Ro' ? 'active' : ''}`}
+            >
+              Personal Ro
+            </button>
+            <button
+              onClick={() => setSelectedMarca('Personal Non UE')}
+              className={`marca-button ${selectedMarca === 'Personal Non UE' ? 'active' : ''}`}
+            >
+              Personal Non UE
+            </button>
+          </div>
+
+          <div className="filter-item">
+            <button onClick={resetFilters}>Reset</button>
           </div>
         </div>
-       )}
+
+        <div className="container">
+          <div className="details container">
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+              {filteredPlaces.length > 0 && filteredPlaces.map(place => (
+                <Link className="link-no-underline" to={"/write/" + place._id} key={place._id}>
+                  <div className="col">
+                    <div className="box card-body p-0 shadow-sm mb-5">
+                      {place.photos.length > 0 && (
+                        <Image
+                          src={place.photos[0]}
+                          className="img-fluid"
+                          style={{ height: "270px", width: "100%", objectFit: "cover" }}
+                        />
+                      )}
+                      <div className="box_content" style={{ display: "contain" }}>
+                        <h4>{place.title}</h4>
+                        <div className="row pl-2 pr-2">
+                          <div>{place.putere}</div>
+                          <button
+                            style={{ background: "#cccccc00", color: "var(--main)" }}
+                            className="btn1"
+                          >
+                            Detalii
+                          </button>
+                          <button
+                            style={{ color: "red" }}
+                            className="btn1"
+                            onClick={(event) => handleDelete(event, place._id)}
+                          >
+                            Sterge
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-       
-    </div>
+    </>
   );
-};
+}
