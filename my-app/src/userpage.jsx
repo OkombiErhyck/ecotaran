1139fcaa2e63fb15db099f1fc8a6e91bbf9c8169
Okombi Erhyck <92018794+OkombiErhyck@users.noreply.github.com>
2025-05-25 17:17:40 +0300

    Update userpage.jsx

--- my-app/src/userpage.jsx
+++ my-app/src/userpage.jsx
@@ -133,6 +133,19 @@ if (redirect) {
                        </div>
                     </div>
 
+                    <div className="col-lg-4 col-xs-6">
+                       <div className="box card-body p-0  shadow-sm mb-5">
+                          
+                           <div className="box_content">
+                           <a href="/miere">
+                           <img src={PlacesPage} className="img-fluid" alt=""/>
+                          
+                               <button className="btn1"> Modifica Flota </button>
+                           </a>
+                           </div>
+                       </div>
+                    </div>
+
 
                     <div className="col-lg-4 col-xs-6">
                        <div className="box card-body p-0  shadow-sm mb-5">
