74714377ef7accb4290d389c6f6823d20b1adc12
Okombi Erhyck <92018794+OkombiErhyck@users.noreply.github.com>
2025-08-25 21:59:05 +0300

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name: String,
+  name: String,          // personal name (e.g. Ion)
+  family: String,        // family grouping (e.g. Popescu)
   email: { type: String, unique: true },
   password: String,
   permissions: {
     type: Map,
     of: Boolean,
-    default: {}, // empty permissions by default
+    default: {},
   },
 });
+
 
 const UserModel = mongoose.model("User", UserSchema);
 
